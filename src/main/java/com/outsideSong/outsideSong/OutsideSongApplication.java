ef010cf67c3953badbfd8a36cf335ef1607b7577
salmon2 <dys04076@naver.com>
2021-12-25 11:59:37 +0900

    first commit

---
+++ src/main/java/com/outsideSong/outsideSong/OutsideSongApplication.java
@@ -0,0 +1,11 @@
+package com.outsideSong.outsideSong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutsideSongApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(OutsideSongApplication.class, args);
+	}
+}
---
+++ src/test/java/com/outsideSong/outsideSong/OutsideSongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.outsideSong.outsideSong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutsideSongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
