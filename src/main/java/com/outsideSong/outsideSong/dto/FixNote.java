35bb9e9f8d75ecd24328acc6cbc9cedbb0f34d92
jhhong0930 <jhhong0930@gmail.com>
2021-12-26 01:18:06 +0900

    update: 음계 수정

--- src/main/java/com/outsideSong/outsideSong/dto/FixNote.java
+++ src/main/java/com/outsideSong/outsideSong/dto/FixNote.java
@@ -6,13 +6,13 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum FixNote {
-    note1("G4", "16n", "#CE3A4A"),
-    note2("A4", "16n", "#F2D230"),
-    note3("B4", "16n", "#85A638"),
-    note4("C4", "16n", "#468263"),
-    note5("D4", "16n", "#5B89A6"),
-    note6("E4", "16n", "#A68160"),
-    note7("F4", "16n", "#C0C0B0"),
+    note1("G5", "16n", "#CE3A4A"),
+    note2("F#5", "16n", "#F2D230"),
+    note3("E5", "16n", "#85A638"),
+    note4("D5", "16n", "#468263"),
+    note5("C5", "16n", "#5B89A6"),
+    note6("B4", "16n", "#A68160"),
+    note7("A4", "16n", "#C0C0B0"),
     note8("G4", "16n", "#F2E2CE");
 
 
